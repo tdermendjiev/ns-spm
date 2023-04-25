aae752a56309ca2370b0cf471145ca5e53c476cf
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-04-25 17:21:14 +0300

    add firebase analytics

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLintBinary",
+    name: "FirebaseAnalytics",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftLintBinary",
-            targets: ["SwiftLintBinary"]),
+            name: "FirebaseAnalytics",
+            targets: ["FirebaseAnalytics"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,10 +18,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // .binaryTarget(
+        //     name: "SwiftLintBinary",
+        //     url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
+        //     checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
+        // )
         .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
+            name: "FirebaseAnalytics",
+            path: "FirebaseAnalytics.xcframework"
         )
         //  .binaryTarget(
         //     name: "NativeScript",
