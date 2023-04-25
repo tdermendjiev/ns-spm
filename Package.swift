35c5e2dab1ce8eb2287f38271221bdb792652342
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-04-25 10:45:31 +0300

    test with other lib

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "NativeScript",
+    name: "SwiftLintBinary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NativeScript",
-            targets: ["NativeScript"]),
+            name: "SwiftLintBinary",
+            targets: ["SwiftLintBinary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,9 +18,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-         .binaryTarget(
-            name: "NativeScript",
-            url: "https://github.com/NativeScript/ios-v8-pod/releases/download/spm-test/NativeScript.xcframework.zip",
-            checksum: "5c6a41ec023b26408ffb6474c6c748bc447958da6a08c9da68138d4841bfb5fe")
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
+        )
+        //  .binaryTarget(
+        //     name: "NativeScript",
+        //     url: "https://github.com/NativeScript/ios-v8-pod/releases/download/spm-test/NativeScript.xcframework.zip",
+        //     checksum: "5c6a41ec023b26408ffb6474c6c748bc447958da6a08c9da68138d4841bfb5fe")
     ]
 )
